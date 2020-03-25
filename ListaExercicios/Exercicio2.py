4b208a979dbf34464f34913bb200d0c377e04d92
ThiagoAnd <thiago.1301801@fapi-pinhais.edu.br>
2020-03-25 01:21:19 -0300

    FEAT: Add - Exercicio
    Adicionado um exercicio

--- ListaExercicios/Exercicio2.py
+++ ListaExercicios/Exercicio2.py
@@ -1,3 +1,10 @@
 import random
 
-print(random.randint(3, 9))
+rand = random.randint(2, 5)
+lista = random.sample(range(1, rand+1), rand)
+print(rand, " ", lista)
+
+for i in range(1,rand+1):
+    for j in range(1, rand -2):
+        print(j, "voltadd")
+    print(i, "volta")
